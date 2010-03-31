2b85d873b570d8e41704d29ae566861f57213a60
kyo116 <kyo116@277b4e66-1657-11df-8e3c-378b6dd812f8>
2010-03-31 07:10:04 +0000

    ExcludeWindow - shows that files which are not within the 2 sync folders as invalid - color coded options - status bar messages at the bottom - remove from the exclude list by clicking

--- v2.0/nsync/ExcludeWindow.xaml.cs
+++ v2.0/nsync/ExcludeWindow.xaml.cs
@@ -21,7 +21,7 @@ namespace nsync
     {
         private string leftPath;
         private string rightPath;
-        private int lastStateItemsCount = 0;
+        //private int lastStateItemsCount = 0;
         private List<string> excludeFolders;
         private List<string> excludeFiles;
         private List<string> excludeFileTypes;
@@ -29,6 +29,8 @@ namespace nsync
         private List<string> oldExcludeFiles;
         private List<string> oldExcludeFileTypes;
         private List<string> availableFileTypes;
+        private List<string> excludeInvalid;
+        private List<string> oldExcludeInvalid;
         private readonly int MAX_STRING_LENGTH = 90;
 
         public ExcludeWindow()
@@ -40,7 +42,9 @@ namespace nsync
             oldExcludeFolders = new List<string>();
             oldExcludeFiles = new List<string>();
             oldExcludeFileTypes = new List<string>();
+            oldExcludeInvalid = new List<string>();
             availableFileTypes = new List<string>();
+            excludeInvalid = new List<string>();
         }
 
         /// <summary>
@@ -149,22 +153,62 @@ namespace nsync
                 string[] fileNames = e.Data.GetData(DataFormats.FileDrop, true) as string[];
                 foreach (string i in fileNames)
                 {
-                    DirectoryInfo dirTemp = new DirectoryInfo(i);
-                    //FileInfo fileTemp = new FileInfo(i);
-                    if (dirTemp.Exists)
+                    if (IsAChildPath(i, leftPath) || IsAChildPath(i, rightPath))
                     {
-                        if(IsNotInList(excludeFolders,i))
-                            excludeFolders.Add(i);
+                        DirectoryInfo dirTemp = new DirectoryInfo(i);
+                        //FileInfo fileTemp = new FileInfo(i);
+                        if (dirTemp.Exists)
+                        {
+                            if (IsNotInList(excludeFolders, i))
+                                excludeFolders.Add(i);
+                        }
+                        else
+                        {
+                            if (IsNotInList(excludeFiles, i))
+                                excludeFiles.Add(i);
+                        }
                     }
                     else
                     {
-                        if (IsNotInList(excludeFiles, i))
-                            excludeFiles.Add(i);
+                        if (IsNotInList(excludeInvalid, i))
+                            excludeInvalid.Add(i);
                     }
                 }
                 ClearListBox();
                 UpdateListBox();
             }
+        }
+
+        private bool IsAChildPath(string childPath, string parentPath)
+        {
+            string[] childPathArray = childPath.Split(new char[] { '\\' });
+            string[] parentPathArray = parentPath.Split(new char[] { '\\' });
+
+            return IsSubPath(childPathArray, parentPathArray);
+        }
+
+        /// <summary>
+        /// Compares the folder paths of the input arrays and determine if they
+        /// have the same path from the root directory.
+        /// </summary>
+        /// <param name="sourceArray">Array of a folder path to be checked</param>
+        /// <param name="destinationArray">Array of a folder path to be checked</param>
+        /// <returns></returns>
+        private bool IsSubPath(string[] sourceArray, string[] destinationArray)
+        {
+            if (sourceArray.Length > destinationArray.Length)
+            {
+                string[] tmp = sourceArray;
+                sourceArray = destinationArray;
+                destinationArray = tmp;
+            }
+
+            for (int i = 0; i < sourceArray.Length; i++)
+            {
+                if (sourceArray[i] != destinationArray[i])
+                    return false;
+            }
+            return true;
         }
 
         private bool IsNotInList(List<string> ExcludeList, string path)
@@ -182,15 +226,28 @@ namespace nsync
             // Setup listbox items
             for (int i = 0; i < excludeFolders.Count; i++)
             {
-                AddListBoxItem("Exclude Folder: " + excludeFolders[i]);
+                AddListBoxItem("Exclude Folder: " + excludeFolders[i], new SolidColorBrush(Colors.SkyBlue), excludeFolders[i]);
             }
             for (int i = 0; i < excludeFiles.Count; i++)
             {
-                AddListBoxItem("Exclude File: " + excludeFiles[i]);
+                AddListBoxItem("Exclude File: " + excludeFiles[i], new SolidColorBrush(Colors.White), excludeFiles[i]);
             }
             for (int i = 0; i < excludeFileTypes.Count; i++)
             {
-                AddListBoxItem("Exclude File Type: " + excludeFileTypes[i]);
+                AddListBoxItem("Exclude File Type: " + excludeFileTypes[i], new SolidColorBrush(Colors.Orange), excludeFileTypes[i]);
+            }
+            for (int i = 0; i < excludeInvalid.Count; i++)
+            {
+                AddListBoxItem("Not in synchronized folders: " + excludeInvalid[i], new SolidColorBrush(Colors.Red), excludeInvalid[i]);
+            }
+
+            if (ListBoxExclude.Items.Count > 0)
+            {
+                LabelStatus.Content = "Click on entries to remove them from the exclude list.";
+            }
+            else
+            {
+                LabelStatus.Content = "Drag & drop files and folders to exclude.";
             }
         }
 
@@ -203,11 +260,13 @@ namespace nsync
             return fullPath;
         }
 
-        private void AddListBoxItem(string excludeStatement)
+        private void AddListBoxItem(string excludeStatement, Brush itemColor, string tag)
         {
             ListBoxItem excludeListBoxItem = new ListBoxItem();
             excludeListBoxItem.Content = ShortenPath(excludeStatement);
             excludeListBoxItem.ToolTip = excludeStatement;
+            excludeListBoxItem.Foreground = itemColor;
+            excludeListBoxItem.Tag = tag;
 
             //listBoxLeft.MouseUp += new MouseButtonEventHandler(ListBoxLeft_MouseUp);
             //listBoxLeft.MouseEnter += new MouseEventHandler(listBoxLeft_MouseEnter);
@@ -233,6 +292,7 @@ namespace nsync
                 CopyList(excludeFolders, oldExcludeFolders);
                 CopyList(excludeFiles, oldExcludeFiles);
                 CopyList(excludeFileTypes, oldExcludeFileTypes);
+                CopyList(excludeInvalid, oldExcludeInvalid);
             }            
         }
 
@@ -264,6 +324,7 @@ namespace nsync
                 CopyList(oldExcludeFolders, excludeFolders);
                 CopyList(oldExcludeFiles, excludeFiles);
                 CopyList(oldExcludeFileTypes, excludeFileTypes);
+                CopyList(oldExcludeInvalid, excludeInvalid);
             //}
 
             ClearListBox();
@@ -282,20 +343,48 @@ namespace nsync
 
         private void ComboBoxFileType_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
-            if (HintText.Visibility == Visibility.Visible)
+            if (ComboBoxFileType.SelectedIndex >= 0)
             {
-                HintText.Visibility = Visibility.Collapsed;
-                HintIcon.Visibility = Visibility.Collapsed;
-                ListBoxExclude.Visibility = Visibility.Visible;
-            }
+                if (HintText.Visibility == Visibility.Visible)
+                {
+                    HintText.Visibility = Visibility.Collapsed;
+                    HintIcon.Visibility = Visibility.Collapsed;
+                    ListBoxExclude.Visibility = Visibility.Visible;
+                }
 
-            ComboBoxItem selectedComboBoxItem = new ComboBoxItem();
-            selectedComboBoxItem = (ComboBoxItem)ComboBoxFileType.SelectedItem;
-            string fileExtension = selectedComboBoxItem.Content.ToString();
-            if (IsNotInList(excludeFileTypes, fileExtension))
-                excludeFileTypes.Add(fileExtension);
-            ClearListBox();
-            UpdateListBox();
+                ComboBoxItem selectedComboBoxItem = new ComboBoxItem();
+                selectedComboBoxItem = (ComboBoxItem)ComboBoxFileType.SelectedItem;
+                string fileExtension = selectedComboBoxItem.Content.ToString();
+                if (IsNotInList(excludeFileTypes, fileExtension))
+                    excludeFileTypes.Add(fileExtension);
+                ClearListBox();
+                UpdateListBox();
+                //This is a fix for the problem of trying to select an already selected item
+                //ComboBoxFileType.SelectedIndex = -1;
+            }
+        }
+
+        private void ListBoxExclude_MouseUp(object sender, MouseButtonEventArgs e)
+        {
+            if (ListBoxExclude.SelectedIndex >= 0)
+            {
+                ListBoxItem selectedListBoxItem = new ListBoxItem();
+                selectedListBoxItem = (ListBoxItem)ListBoxExclude.SelectedItem;
+                string path = selectedListBoxItem.Tag.ToString();
+                ListBoxExclude.SelectedIndex = -1;
+
+                excludeFolders.Remove(path);
+                excludeFiles.Remove(path);
+                excludeFileTypes.Remove(path);
+                excludeInvalid.Remove(path);
+
+                ClearListBox();
+                UpdateListBox();
+
+                //ListBoxExclude.Items.Remove(ListBoxExclude.SelectedItem);
+                //ListBoxExclude.SelectedIndex = -1;
+
+            }
         }
     }
 }
