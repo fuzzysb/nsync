ca25bdb0a72c7a9f87e8bada3b9e244be2682bcc
seahsq <seahsq@277b4e66-1657-11df-8e3c-378b6dd812f8>
2010-03-31 17:52:48 +0000

    Tagging V1.0

---
+++ nsync/Intelligence.cs
@@ -0,0 +1,92 @@
+﻿using System.IO;
+using System.Management;
+
+namespace nsync
+{
+    class Intelligence
+    {
+        /// <summary>
+        /// Checks if the folder path exists
+        /// </summary>
+        /// <param name="folderPath">This parameter is the folder path to be checked</param>
+        /// <returns>Returns a boolean which indicates if the folder path exists</returns>
+        public bool IsFolderExists(string folderPath)
+        {
+            if (Directory.Exists(folderPath))
+                return true;
+            else
+                return false;
+        }
+
+        /// <summary>
+        /// Checks if two folder paths are similar
+        /// </summary>
+        /// <param name="leftFolderPath">This parameter is the folder path to be checked</param>
+        /// <param name="rightFolderPath">This parameter is the folder path to be checked</param>
+        /// <returns>Returns a boolean which indicates if both folder paths are similar</returns>
+        public bool IsFoldersSimilar(string leftFolderPath, string rightFolderPath)
+        {
+            if (leftFolderPath == rightFolderPath)
+                return true;
+            return false;
+        }
+
+        /// <summary>
+        /// Checks if a folder is a subfolder of another
+        /// </summary>
+        /// <param name="leftFolderPath">This parameter is the folder path to be checked</param>
+        /// <param name="rightFolderPath">This parameter is the folder path to be checked</param>
+        /// <returns>Returns a boolean which indicates if a folder is a subfolder of another</returns>
+        public bool IsFolderSubFolder(string leftFolderPath, string rightFolderPath)
+        {
+            string[] leftFolderPathArray = leftFolderPath.Split(new char[] { '\\' });
+            string[] rightFolderPathArray = rightFolderPath.Split(new char[] { '\\' });
+
+            return isSamePath(leftFolderPathArray, rightFolderPathArray);
+        }
+
+        /// <summary>
+        /// Compares the folder paths of the input arrays and determine if they
+        /// have the same path from the root directory.
+        /// </summary>
+        /// <param name="sourceArray">Array of a folder path to be checked</param>
+        /// <param name="destinationArray">Array of a folder path to be checked</param>
+        /// <returns></returns>
+        private bool isSamePath(string[] sourceArray, string[] destinationArray)
+        {
+            if (sourceArray.Length > destinationArray.Length)
+            {
+                string[] tmp = sourceArray;
+                sourceArray = destinationArray;
+                destinationArray = tmp;
+            }
+
+            for (int i = 0; i < sourceArray.Length; i++)
+            {
+                if (sourceArray[i] != destinationArray[i])
+                    return false;
+            }
+            return true;
+        }
+
+        /// <summary>
+        /// Checks if the folder path belongs to a removable drive
+        /// </summary>
+        /// <param name="path">This parameter is the folder path to be checked</param>
+        /// <returns>Returns a boolean which indicates if the folder path belongs to a removable drive</returns>
+        public bool IsRemovableDrive(string path)
+        {
+            ManagementObjectSearcher mosDisks = new ManagementObjectSearcher(@"SELECT * FROM Win32_LogicalDisk WHERE DriveType=2"); //Finds all removable drives
+
+            foreach (ManagementObject moDisk in mosDisks.Get())
+            {
+                if (moDisk["DeviceID"].ToString() == Directory.GetDirectoryRoot(path).Remove(2))
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+    }
+}
