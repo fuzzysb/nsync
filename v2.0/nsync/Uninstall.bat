ed2cf689d6846d0e4ff253f83a61098c7e9c6e3e
seahsq <seahsq@277b4e66-1657-11df-8e3c-378b6dd812f8>
2010-04-05 11:13:24 +0000

    Deleting this version of v2.0 trunk. svn giving me problems in committing.

--- v2.0/nsync/Uninstall.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-
-DEL "%PROGRAMFILES%\TEAM14\NSYNC\SETTINGS.XML" /s /q
-
-DEL "%localappdata%\VirtualStore\Program Files\team14\nsync\settings.xml" /s /q
-
-msiexec /x {4F2F382E-3D60-4304-8D3C-68A808D9C1DB}
