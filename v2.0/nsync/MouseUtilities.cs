f0f259534c4d979f09f7ba81ed8ccc7248c63b5b
kyo116 <kyo116@277b4e66-1657-11df-8e3c-378b6dd812f8>
2010-04-12 14:40:28 +0000

    -Added comments

--- v2.0/nsync/MouseUtilities.cs
+++ v2.0/nsync/MouseUtilities.cs
@@ -7,6 +7,11 @@ namespace nsync
 {
     public static class MouseUtilities
     {
+        /// <summary>
+        /// Win32 API to get the correct position of the mouse point on a drag event
+        /// </summary>
+        /// <param name="relativeTo">coordinates relative to an WPF control</param>
+        /// <returns>returns the coordinates as a point object</returns>
         public static Point CorrectGetPosition(Visual relativeTo)
         {
             Win32Point w32Mouse = new Win32Point();
@@ -14,6 +19,9 @@ namespace nsync
             return relativeTo.PointFromScreen(new Point(w32Mouse.X, w32Mouse.Y));
         }
 
+        /// <summary>
+        /// A Win32 point with x,y coordinates
+        /// </summary>
         [StructLayout(LayoutKind.Sequential)]
         internal struct Win32Point
         {
@@ -21,6 +29,11 @@ namespace nsync
             public Int32 Y;
         };
 
+        /// <summary>
+        /// imported Win32 API for getting cursor position
+        /// </summary>
+        /// <param name="pt"></param>
+        /// <returns></returns>
         [DllImport("user32.dll")]
         [return: MarshalAs(UnmanagedType.Bool)]
         internal static extern bool GetCursorPos(ref Win32Point pt);
