06f0512cf459576c4cca911049cc82ced8817803
kyo116 <kyo116@277b4e66-1657-11df-8e3c-378b6dd812f8>
2010-04-14 15:59:48 +0000

    -added comments

--- v2.0/nsync/ExtensionMethods.cs
+++ v2.0/nsync/ExtensionMethods.cs
@@ -3,16 +3,16 @@
 namespace nsync
 {
     /// <summary>
-    /// 
+    /// extension methods class
     /// </summary>
     public static class ExtensionMethods
     {
         /// <summary>
-        /// 
+        /// extension method to check if an item is in a string array
         /// </summary>
-        /// <param name="stringList"></param>
-        /// <param name="item"></param>
-        /// <returns></returns>
+        /// <param name="stringList">the list which will have the contains method</param>
+        /// <param name="item">the string to check for in the list</param>
+        /// <returns>true if its in the list</returns>
         public static bool Contains(this string[] stringList, string item)
         {
             foreach (string s in stringList)
